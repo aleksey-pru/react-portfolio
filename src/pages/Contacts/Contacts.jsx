02b1de398a92d02e903056dd15ef36cee51a5e38
Oleksii <o.pruzhanskiy@gmail.com>
2025-05-29 11:02:39 +0300

    fix

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -14,7 +14,7 @@ const Contacts = () => {
   };
   return (
     <main className="bg-zinc-200 dark:bg-zinc-900 py-[70px] flex ">
-      <div className="text-zinc-900 dark:text-white max-w-[1200px] mx-auto px-4">
+      <div className="text-zinc-900 dark:text-white w-md mx-auto px-4">
         <h2 className="mb-[60px] text-[50px] font-bold leading-[1.3] text-[var(--title-1)] text-center">
           Contact Me
         </h2>
@@ -46,31 +46,6 @@ const Contacts = () => {
             </button>
           </form>
         </section>
-
-        <ul className="max-w-[570px] mx-auto flex flex-col items-center gap-[40px] text-center">
-          <li className="text-[18px] leading-[1.5]">
-            <h2 className="mb-[20px] text-[40px] font-bold leading-[1.3]">
-              Location
-            </h2>
-            <p>Ukraine</p>
-          </li>
-          <li className="text-[18px] leading-[1.5]">
-            <h2 className="mb-[20px] text-[40px] font-bold leading-[1.3]">
-              Telegram / WhatsApp
-            </h2>
-            <p>
-              <a href="tel:+79051234567">+</a>
-            </p>
-          </li>
-          <li className="text-[18px] leading-[1.5]">
-            <h2 className="mb-[20px] text-[40px] font-bold leading-[1.3]">
-              Email
-            </h2>
-            <p>
-              <a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a>
-            </p>
-          </li>
-        </ul>
       </div>
     </main>
   );
